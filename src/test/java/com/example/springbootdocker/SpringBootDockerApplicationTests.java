87731c060c72cad3ab327be9980e49c640e0e18d
madeirauno <noize.ji@gmail.com>
2021-02-09 12:00:07 +0200

    add lombok and test logging

--- src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.springbootdocker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringBootDockerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("some test done");
 	}
 
 }
