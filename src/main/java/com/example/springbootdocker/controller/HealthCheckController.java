0527b9d141d7bb9d7331742a73a0310519f7d753
madeirauno <noize.ji@gmail.com>
2021-02-16 16:37:04 +0200

    change github actions workflow, add property endpoint for task

--- src/main/java/com/example/springbootdocker/controller/HealthCheckController.java
+++ src/main/java/com/example/springbootdocker/controller/HealthCheckController.java
@@ -1,15 +1,25 @@
 package com.example.springbootdocker.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
+    @Value("${endpoint-message}")
+    private String message;
+
     @Operation(summary = "Simple health check endpoint")
     @GetMapping("/check")
     public String healthCheckEndpoint (){
         return "OK!";
+    }
+
+    @Operation(summary = "Property message check endpoint")
+    @GetMapping("/message")
+    public String propertyMessageEndpoint (){
+        return String.format("Message is: %s", message);
     }
 }
