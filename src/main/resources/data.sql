ad6a783eaedefeb5f83e1fb32df0d952b314de11
madeirauno <noize.ji@gmail.com>
2021-03-22 16:30:48 +0200

    check deployment

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO products (name, price) VALUES ('meat', '5'), ('cheese', '2'), ('eggs', '3'), ('milk', '1');
-INSERT INTO categories (name, product_count) VALUES ('food', '0'), ('beverages', '0');
